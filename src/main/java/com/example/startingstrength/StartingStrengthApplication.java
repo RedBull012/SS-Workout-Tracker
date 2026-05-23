e358fec7ff9d2de6f07521362de24f66bb8223df
Michael-Sauer1 <msauer012@gmail.com>
2026-05-22 22:45:19 -0400

    second commit

---
+++ src/main/java/com/example/startingstrength/StartingStrengthApplication.java
@@ -0,0 +1,13 @@
+package com.example.startingstrength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingStrengthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartingStrengthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/startingstrength/StartingStrengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startingstrength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingStrengthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
